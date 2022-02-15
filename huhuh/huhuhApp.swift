ac032733f062bfdde4552a659e80b1fc887ff3f7
higashi <s07307384@yahoo.co.jp>
2022-02-16 01:07:10 +0900

    Initial Commit

---
+++ huhuh/huhuhApp.swift
@@ -0,0 +1,17 @@
+//
+//  huhuhApp.swift
+//  huhuh
+//
+//  Created by higashi on 2022/02/16.
+//
+
+import SwiftUI
+
+@main
+struct huhuhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
